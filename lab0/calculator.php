7b007c30c4fffacbd4e3d2b23e314b9a8a968f85
Farenheit <tiger.mananticot@yandex.ru>
2022-09-17 13:39:18 +0300

    Tried to see how explode() works

--- lab0/calculator.php
+++ lab0/calculator.php
@@ -1,9 +1,10 @@
 <?php
     function calculator(string $str) : int{
-        echo('opa');
-        if(strpos($str, '+')){
-            
-        }
+        $numbers = explode("+",$str,);
+        $numbers2 = explode("-", $str);
+        var_dump($numbers);
+        var_dump($numbers2);
         return 0;
     }
+    calculator("3+3+5-1+4");
 ?>
