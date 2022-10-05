eb8b9f6372af4eee23ee5b3c30e346523a2e3355
Farenheit <tiger.mananticot@yandex.ru>
2022-10-05 20:34:23 +0300

    Time input and errors processing

--- lab0/sumTime.php
+++ lab0/sumTime.php
@@ -1,1 +1,31 @@
 <?php
+function isValidInput(string $time) : bool{
+  return ($time[0] >= 24 || $time[0] < 0 || $time[1] > 59 || $time[1] < 0 || $time[2] > 59 || $time[2] < 0) ? false : true;
+}
+
+function sumTime(string $first, string $second) : string{
+  $firstTime = explode(':', $first);
+  $secondTime = explode(':', $second);
+  $sumTime = array();
+
+  if(!isValidInput($first) || !isValidInput($second)){
+    return 'Ошибка ввода!';
+  }
+
+  $validChars = ' 0123456789:';
+  for($i = 0; $i < strlen($first); $i++){
+    if(!strpos($validChars, $first[$i])){
+      return 'Ошибка ввода';
+    }
+  }
+  for($i = 0; $i < strlen($second); $i++){
+    if(!strpos($validChars, $second[$i])){
+      return 'Ошибка ввода';
+    }
+  }
+
+  $sumTime = $firstTime;
+  var_dump($sumTime);
+  return $sumTime[0];
+}
+echo sumTime('22:59:59', '19:04:22');
