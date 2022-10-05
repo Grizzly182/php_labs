9a6cd306255049bf8538a5c3a254a490d00f6118
Farenheit <tiger.mananticot@yandex.ru>
2022-10-05 21:38:22 +0300

    finished and added command propt integration

--- lab0/sumTime.php
+++ lab0/sumTime.php
@@ -1,31 +1,63 @@
 <?php
-function isValidInput(string $time) : bool{
-  return ($time[0] >= 24 || $time[0] < 0 || $time[1] > 59 || $time[1] < 0 || $time[2] > 59 || $time[2] < 0) ? false : true;
+//Работу выполнил студент группы П-31
+//Белоусов Михаил
+function isValidInput(string $time): bool
+{
+    return ($time[0] >= 24 || $time[0] < 0 || $time[1] > 59 || $time[1] < 0 || $time[2] > 59 || $time[2] < 0) ? false : true;
 }
 
-function sumTime(string $first, string $second) : string{
-  $firstTime = explode(':', $first);
-  $secondTime = explode(':', $second);
-  $sumTime = array();
+function sumTime(string $first, string $second): string
+{
+    $firstTime = explode(':', $first);
+    $secondTime = explode(':', $second);
+    $sumTime = array();
 
-  if(!isValidInput($first) || !isValidInput($second)){
-    return 'Ошибка ввода!';
-  }
-
-  $validChars = ' 0123456789:';
-  for($i = 0; $i < strlen($first); $i++){
-    if(!strpos($validChars, $first[$i])){
-      return 'Ошибка ввода';
+    if (!isValidInput($first) || !isValidInput($second) || count($firstTime) !== 3 || count($secondTime) !== 3) {
+        return 'Ошибка ввода!';
     }
-  }
-  for($i = 0; $i < strlen($second); $i++){
-    if(!strpos($validChars, $second[$i])){
-      return 'Ошибка ввода';
-    }
-  }
 
-  $sumTime = $firstTime;
-  var_dump($sumTime);
-  return $sumTime[0];
+    $validChars = ' 0123456789:';
+    for ($i = 0; $i < strlen($first); $i++) {
+        if (!strpos($validChars, $first[$i])) {
+            return 'Ошибка ввода';
+        }
+    }
+    for ($i = 0; $i < strlen($second); $i++) {
+        if (!strpos($validChars, $second[$i])) {
+            return 'Ошибка ввода';
+        }
+    }
+
+    $sumTime = $firstTime;
+
+    //Секудны
+    if ($sumTime[2] + $secondTime[2] >= 60) {
+        $temp = 60 - $sumTime[2];
+        $sumTime[2] = $secondTime[2] - $temp;
+        $sumTime[1]++;
+    } else {
+        $sumTime[2] += $secondTime[2];
+    }
+
+    //Минуты
+    if ($sumTime[1] + $secondTime[1] >= 60) {
+        $temp = 60 - $sumTime[1];
+        $sumTime[1] = $secondTime[1] - $temp;
+        $sumTime[0]++;
+    } else {
+        $sumTime[1] += $secondTime[1];
+    }
+
+    //Часы
+    if ($sumTime[0] + $secondTime[0] >= 24) {
+        $temp = 24 - $sumTime[0];
+        $sumTime[0] = $secondTime[0] - $temp;
+    } else {
+        $sumTime[0] += $secondTime[0];
+    }
+
+    return $sumTime[0] . ':' . $sumTime[1] . ':' . $sumTime[2];
 }
-echo sumTime('22:59:59', '19:04:22');
+if ($argv[1] !== null && $argv[2] !== null) {
+    echo sumTime($argv[1], $argv[2]);
+}
