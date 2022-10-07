5b03ac4ee8e8dbf3ba52af2b0d98307e9153a2d3
Farenheit <tiger.mananticot@yandex.ru>
2022-10-07 09:20:27 +0300

    everything's done

--- lab1/Calculator.php
+++ lab1/Calculator.php
@@ -1,4 +1,41 @@
 <?php
-class Calculator{
-    
+class Calculator
+{
+    private float $result;
+
+    public function __construct()
+    {
+        $this->result = 0;
+    }
+
+    public function sum(float $number)
+    {
+        $this->result += $number;
+        return $this;
+    }
+
+    public function minus(float $number)
+    {
+        $this->result -= $number;
+        return $this;
+    }
+
+    public function product(float $number)
+    {
+        $this->result *= $number;
+        return $this;
+    }
+
+    public function division(float $number)
+    {
+        $number !== 0.0 ? $this->result /= $number : $this->result = 0;
+        return $this;
+    }
+
+    public function getResult()
+    {
+        $tempResult = $this->result;
+        $this->result = 0;
+        return $tempResult;
+    }
 }
--- lab1/task1.php
+++ lab1/task1.php
@@ -1,2 +1,7 @@
 <?php
+require('Calculator.php');
 $calculator = new Calculator();
+echo $calculator->sum(4)->minus(1)->product(42)->division(0.3)->getResult() . PHP_EOL; // 420
+echo $calculator->sum(3)->sum(3)->minus(3)->division(3)->getResult() . PHP_EOL; // 1
+echo $calculator->sum(1.4)->sum(2.6)->product(4)->getResult() . PHP_EOL; // 16
+echo $calculator->sum(1)->sum(2)->product(3)->division(0)->getResult(); // 0
