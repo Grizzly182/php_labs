5a1e964b3f39cbeb76f4ab2daa180f8a0f5caceb
Mikhail Belousov <tiger.mananticot@yandex.ru>
2022-10-09 23:19:45 +0300

    refactored more

--- lab1/Calculator.php
+++ lab1/Calculator.php
@@ -5,25 +5,25 @@ class Calculator
 {
     private float $result;
 
-    public function sum(float $number) : Calculator
+    public function sum(float $number) : self
     {
         $this->result += $number;
         return $this;
     }
 
-    public function minus(float $number) : Calculator
+    public function minus(float $number) : self
     {
         $this->result -= $number;
         return $this;
     }
 
-    public function product(float $number) : Calculator
+    public function product(float $number) : self
     {
         $this->result *= $number;
         return $this;
     }
 
-    public function division(float $number) : Calculator
+    public function division(float $number) : self
     {
         $number !== 0.0 ? $this->result /= $number : $this->result = 0;
         return $this;
