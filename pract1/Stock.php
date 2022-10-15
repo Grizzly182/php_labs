aabd7004480a73c7e5c8af91a00c54d0104c34b4
Mikhail Belousov <tiger.mananticot@yandex.ru>
2022-10-15 19:10:15 +0300

    refactored tabs

--- pract1/Stock.php
+++ pract1/Stock.php
@@ -1,17 +1,20 @@
 <?php
-  class Stock{
+class Stock
+{
     private int $countFood = 0;
 
-    public function getCountFood(): int{
-      return $this->countFood;
+    public function getCountFood(): int
+    {
+        return $this->countFood;
     }
 
-    public function addFood(int $countFood) : void{
-      $this->countFood += $countFood;
+    public function addFood(int $countFood): void
+    {
+        $this->countFood += $countFood;
     }
 
-    public function removeFood(int $countFood) : void{
-      $this->countFood -= $countFood;
+    public function removeFood(int $countFood): void
+    {
+        $this->countFood -= $countFood;
     }
-  }
-?>
+}
