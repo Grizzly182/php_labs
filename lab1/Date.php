4e70e714ab72d7b0c32b026d38bccbbfca22ea97
Mikhail Belousov <tiger.mananticot@yandex.ru>
2022-10-15 19:09:30 +0300

    everything is working fine

--- lab1/Date.php
+++ lab1/Date.php
@@ -21,30 +21,45 @@ class Date
     {
         switch ($format) {
             case 'ru':
-                return "{$this->day}.{$this->month}.{$this->year}" . PHP_EOL;
+                return "{$this->day}.{$this->month}.{$this->year}";
             case 'en':
-                return "{$this->year}-{$this->month}-{$this->day}" . PHP_EOL;
+                return "{$this->year}-{$this->month}-{$this->day}";
             default:
                 throw new Exception('Неверный формат.');
         }
     }
 
-    public function diffDay(Date $secondDate): int
+    public function diffDay(Date $secondDate): string
     {
-        $firstDate = date_create($this->format('ru'));
-        $secondDate = date_create($secondDate->format('ru'));
+        $firstDate = date_create($this->format('en'));
+        $secondDate = date_create($secondDate->format('en'));
         $difference = $firstDate->diff($secondDate);
-        return (int)$difference->format('%a');
+        return $difference->format('%a');
+    }
+
+    public function minusDay(int $daysAmount, string $format): string
+    {
+        $formatString = '';
+        switch ($format) {
+            case 'ru':
+                $formatString = 'd.m.Y';
+                break;
+            case 'en':
+                $formatString = 'Y-m-d';
+                break;
+            default:
+                throw new Exception('Неверный формат.');
+        }
+        return date($formatString, strtotime("-{$daysAmount} days", strtotime($this->format('ru'))));
+    }
+
+    public function getDayOfWeek(): string
+    {
+        return date('l', strtotime($this->format('ru')));
     }
 
     private static function isValidDateInput(Date $date): bool
     {
         return !(($date->day > 31 || $date->day <= 0) || ($date->month > 12 || $date->month <= 0) || ($date->year < 0));
-    }
-
-    private static function convertToStringDate(Date $date) : string
-    {
-        (string)$stringDate = "{$date->day}.{$date->month}.{$date->year}";
-        return $stringDate;
     }
 }
