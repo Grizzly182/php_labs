6678db66da230d465c94c026341943037c637981
Mikhail <belousovmihailres@gmail.com>
2022-12-14 20:28:04 +0300

    extended and refactored

--- pract3/task3/Disk.php
+++ pract3/task3/Disk.php
@@ -11,19 +11,49 @@ class Disk extends OSComponent
     public function display(): void
     {
         echo $this->name . PHP_EOL;
-        foreach($this->components as $component){
+        foreach ($this->components as $component) {
             echo "\t" . $component->display();
         }
     }
 
-    public function displayWholeDisk() : void{
+    public function displayNumberOfFilesAndDirectories(): void
+    {
+        $filesAmount = 0;
+        $directoriesAmount = 0;
+        $tempDirectoryFiles = [];
+        $finalComponents = [];
+        foreach ($this->components as $component) {
+            $type = $component->getType();
+            if ($type === 'PHPDirectory') {
+                $directoriesAmount++;
+                $tempDirectoryFiles += $component->getComponents();
+            } else {
+                $filesAmount++;
+            }
 
+            if (count($tempDirectoryFiles) !== 0) {
+                foreach ($tempDirectoryFiles as $component) {
+                    if ($component->getType() === 'File') {
+                        $filesAmount++;
+                    } else if ($component->getType() === 'PHPDirectory') {
+                        $finalComponents = $component->getComponentsTypes();
+                    }
+                }
+            }
+        }
+        foreach ($finalComponents as $component) {
+            if ($component === 'File') {
+                $filesAmount++;
+            } else
+                $directoriesAmount++;
+        }
+        echo "Диск $this->name" . PHP_EOL . "$filesAmount Файл(-ов)" . PHP_EOL . "$directoriesAmount Папки(-ок)" . PHP_EOL;
     }
 
     public function count(): int
     {
         $sum = 0;
-        foreach($this->components as $component){
+        foreach ($this->components as $component) {
             $sum += $component->count();
         }
         return $sum;
@@ -31,9 +61,10 @@ class Disk extends OSComponent
 
     public function add(OSComponent $component): void
     {
-        if(get_class($component) === 'Disk'){
+        if (get_class($component) === 'Disk') {
             throw new InvalidArgumentException('Disk cannot have another Disk in it');
         }
         $this->components[] = $component;
     }
+
 }
